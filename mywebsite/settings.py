9d8d2f3a145737303b5e195aeb7ebf0cf3417153
ramadhanappp <ramadhanappp@gmail.com>
2021-08-11 08:56:12 +0700

    teledor

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -14,12 +14,17 @@ from pathlib import Path
 import os
 import dj_database_url
 
+# Create your views here.
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PRODUCTION = os.environ.get('DATABASE URL') is not None
+PRODUCTION = os.environ.get('DATABASE_URL') is not None
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-&7i%=#6=3=3q-y=fetla3l2@cs6!1*pg(7x5e91pii&zg37y7('
@@ -27,7 +32,7 @@ SECRET_KEY = 'django-insecure-&7i%=#6=3=3q-y=fetla3l2@cs6!1*pg(7x5e91pii&zg37y7(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -128,7 +133,9 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
