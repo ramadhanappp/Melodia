f0818bac08caaea855ba645245c480e5be14c0a5
ramadhanappp <ramadhanappp@gmail.com>
2021-08-11 10:16:36 +0700

    status

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -3,11 +3,11 @@ from django.urls.resolvers import URLPattern
 from . import views
 
 urlpatterns = [
-    path('secondpage', views.second_page),
-    path('firstpage', views.first_page),
+    path('secondpage/', views.secondpage),
+    path('firstpage/', views.firstpage),
     path('rama', views.rama),
-    path('example', views.example),
+    path('example/', views.example),
     path('count/<int:angka>/', views.count),
-    path('second', views.second_page),
+    path('second', views.secondpage),
     path('', views.landing_page),
 ]
