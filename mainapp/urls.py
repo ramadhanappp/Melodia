67c19426eaa0ec133a637d35bcb3610ae259a412
ramadhanappp <ramadhanappp@gmail.com>
2021-08-11 10:40:00 +0700

    yes udah bisa

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -3,11 +3,8 @@ from django.urls.resolvers import URLPattern
 from . import views
 
 urlpatterns = [
-    path('secondpage/', views.secondpage),
-    path('firstpage/', views.firstpage),
-    path('rama', views.rama),
-    path('example/', views.example),
-    path('count/<int:angka>/', views.count),
-    path('second', views.secondpage),
+    path('woman', views.woman),
+    path('example', views.example),
+    path('second', views.second_page),
     path('', views.landing_page),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -6,30 +6,16 @@ from django.http import HttpResponse
 
 
 def landing_page(request):
-    return HttpResponse('hello world')
+    return render(request, "firstpage.html")
 
 
 def second_page(request):
-    return HttpResponse("second page")
-
-
-def count(request):
-    angka = 0
-    angka = angka+1
-    return HttpResponse(str(angka))
+    return render(request, "secondpage.html")
 
 
 def example(request):
     return render(request, 'example.html')
 
 
-def rama(request):
-    return HttpResponse("Hallo brow")
-
-
-def firstpage(request):
-    return render(request, 'firstpage.html')
-
-
-def secondpage(request):
-    return render(request, 'secondpage.html')
+def woman(request):
+    return(request, 'woman.html')
